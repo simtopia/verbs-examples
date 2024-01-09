f2add583f8f1db3f43e13723bce6d0ecebdab7c9
Marc Sabaté <msabvid@gmail.com>
2024-01-09 11:24:46 +0000

    first commit

---
+++ agents/uniswap_agent.py
@@ -0,0 +1,220 @@
+import numpy as np
+import verbs
+
+
+class Gbm:
+    def __init__(
+        self, mu: float, sigma: float, token_a_price: int, token_b_price: int, dt: float
+    ):
+        self.mu = mu
+        self.sigma = sigma
+        self.token_a_price = token_a_price
+        self.token_b_price = token_b_price
+        self.dt = dt
+
+    def update(
+        self,
+        rng: np.random.Generator,
+    ):
+        """
+        Update Gbm:
+        - P^a_{t+dt} = P^a_t * exp((mu-0.5*sigma^2)dt + sigma * (W_{t+dt} - W_{t}))
+        - P^b is constant
+
+        """
+        z = rng.normal()
+        new_price_a = self.token_a_price * np.exp(
+            (self.mu - 0.5 * self.sigma**2) * self.dt
+            + self.sigma * np.sqrt(self.dt) * z
+        )
+
+        # update price values
+        self.token_a_price = int(new_price_a)
+
+    def get_sqrt_price_token_a_x96(self):
+        price = self.token_a_price / self.token_b_price
+        return np.sqrt(price) * 2**96
+
+    def get_sqrt_price_token_b_x96(self):
+        price = self.token_b_price / self.token_a_price
+        return np.sqrt(price) * 2**96
+
+    def get_price_token_a(self):
+        price = self.token_a_price / self.token_b_price
+        return price
+
+
+class UniswapAgent:
+    def __init__(
+        self,
+        network,
+        i: int,
+        swap_router_abi,
+        uniswap_pool_abi,
+        fee: int,
+        swap_router_address: str,
+        uniswap_pool_address: str,
+        token_a_address: str,  # external market (gbm) returns the price of token a in terms of token b
+        token_b_address: str,  # token b is the numeraire
+        token_a_price: int,
+        token_b_price: int,
+        mu: float,
+        sigma: float,
+        dt: float,
+    ):
+        self.net = network
+        self.address = verbs.utils.int_to_address(i)
+        self.net.create_account(self.address, int(1e25))
+        self.swap_router_abi = swap_router_abi
+        self.uniswap_pool_abi = uniswap_pool_abi
+        self.swap_router_address = verbs.utils.hex_to_bytes(swap_router_address)
+        self.uniswap_pool_address = verbs.utils.hex_to_bytes(uniswap_pool_address)
+        self.uniswap_fee = fee
+        self.weth_address = token_a_address
+        self.dai_address = token_b_address
+
+        self.token_b = token_b_address  # stablecoin.
+        self.token0_address = self.uniswap_pool_abi.token0.call(
+            self.net, self.net.admin_address, self.uniswap_pool_address, []
+        )[0][0]
+        self.token1_address = self.uniswap_pool_abi.token1.call(
+            self.net, self.net.admin_address, self.uniswap_pool_address, []
+        )[0][0]
+        self.fee = fee
+
+        # external market model
+        self.external_market = Gbm(
+            mu=mu,
+            sigma=sigma,
+            token_a_price=token_a_price,
+            token_b_price=token_b_price,
+            dt=dt,
+        )
+
+        # step of simulator
+        self.step = 0
+
+    def get_swap_size_to_increase_uniswap_price(
+        self,
+        sqrt_price_external_market_x96: int,
+        sqrt_price_uniswap_x96: int,
+        liquidity: int,
+    ):
+        change_sqrt_price_x96 = sqrt_price_external_market_x96 - sqrt_price_uniswap_x96
+        change_token_1 = int(liquidity * change_sqrt_price_x96 / 2**96)
+        if change_token_1 > 0:
+            swap = self.swap_router_abi.exactInputSingle.transaction(
+                self.address,
+                self.swap_router_address,
+                [
+                    (
+                        self.token1_address,
+                        self.token0_address,
+                        self.fee,
+                        self.address,
+                        10**32,
+                        change_token_1,
+                        0,
+                        0,
+                    )
+                ],
+            )
+            return swap
+        else:
+            return None
+
+    def get_swap_size_to_decrease_uniswap_price(
+        self,
+        sqrt_price_external_market_x96: int,
+        sqrt_price_uniswap_x96: int,
+        liquidity: int,
+    ):
+        change_sqrt_price_x96 = sqrt_price_uniswap_x96 - sqrt_price_external_market_x96
+        change_token_1 = int(liquidity * change_sqrt_price_x96 / 2**96)
+        if change_token_1 > 0:
+            swap = self.swap_router_abi.exactOutputSingle.transaction(
+                self.address,
+                self.swap_router_address,
+                [
+                    (
+                        self.token0_address,
+                        self.token1_address,
+                        self.fee,
+                        self.address,
+                        10**32,
+                        change_token_1,
+                        10**32,
+                        0,
+                    )
+                ],
+            )
+            return swap
+        else:
+            return None
+
+    def update(self, rng: np.random.Generator, network):
+        # get sqrt price from uniswap pool. Uniswap returns price of token0 in terms of token1
+        slot0 = self.uniswap_pool_abi.slot0.call(
+            self.net, self.address, self.uniswap_pool_address, []
+        )[0]
+        sqrt_price_uniswap_x96 = slot0[0]
+
+        # get liquidity from uniswap pool
+        liquidity = self.uniswap_pool_abi.liquidity.call(
+            self.net, self.address, self.uniswap_pool_address, []
+        )[0][0]
+
+        # external market update
+        self.external_market.update(rng)
+
+        if self.token_b == self.token1_address:
+            sqrt_price_external_market_x96 = (
+                self.external_market.get_sqrt_price_token_a_x96()
+            )
+        else:
+            sqrt_price_external_market_x96 = (
+                self.external_market.get_sqrt_price_token_b_x96()
+            )
+
+        # find encoded swap params so that price of uniswap after swap matches the price of the external market
+        # sqrt_price_external_market > sqrt_price_uniswap_x96, the uniswap agent wants to buy collateral asset (and sell debt asset) to increase the price of Uniswap
+        # sqrt_price_external_market < sqrt_price_uniswap_x96, the uniswap agent wants to sell collateral asset (and buy debt asset) to decrease the price of Uniswap
+        if sqrt_price_external_market_x96 > sqrt_price_uniswap_x96:
+            swap_call = self.get_swap_size_to_increase_uniswap_price(
+                sqrt_price_external_market_x96=sqrt_price_external_market_x96,
+                sqrt_price_uniswap_x96=sqrt_price_uniswap_x96,
+                liquidity=liquidity,
+            )
+        else:
+            swap_call = self.get_swap_size_to_decrease_uniswap_price(
+                sqrt_price_external_market_x96=sqrt_price_external_market_x96,
+                sqrt_price_uniswap_x96=sqrt_price_uniswap_x96,
+                liquidity=liquidity,
+            )
+        self.step += 1
+
+        if swap_call is not None:
+            return [swap_call]
+        else:
+            return []
+
+    def record(self):
+        # get sqrt price from uniswap pool. Uniswap returns price of token0 in terms of token1
+        slot0 = self.uniswap_pool_abi.slot0.call(
+            self.net, self.net.admin_address, self.uniswap_pool_address, []
+        )[0]
+        sqrt_price_uniswap_x96 = slot0[0]
+
+        if self.token_b == self.token1_address:
+            sqrt_price_external_market_x96 = (
+                self.external_market.get_sqrt_price_token_a_x96()
+            )
+        else:
+            sqrt_price_external_market_x96 = (
+                self.external_market.get_sqrt_price_token_b_x96()
+            )
+
+        sqrt_price_uniswap = sqrt_price_uniswap_x96 / 2**96
+        sqrt_price_external_market = sqrt_price_external_market_x96 / (2**96)
+
+        return (sqrt_price_uniswap**2, sqrt_price_external_market**2)
---
+++ uniswap_gbm.py
@@ -0,0 +1,108 @@
+"""
+In this example 
+"""
+
+
+import numpy as np
+import verbs
+import argparse
+from agents.uniswap_agent import UniswapAgent
+
+WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+DAI_ADMIN = "0x9759A6Ac90977b93B58547b4A71c78317f391A28"
+UNISWAP_V3_FACTORY = "0x1F98431c8aD98523631AE4a59f267346ea31F984"
+UNISWAP_WETH_DAI = "0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8"  # sanity check, obtained from the factory contract using web3.py
+SWAP_ROUTER = "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", type=str, help="prive key from Alchemy")
+    parser.add_argument(
+        "--block", type=int, default=18784000, help="Ethereum block number"
+    )
+
+    args = parser.parse_args()
+    key = args.key
+    block_number = args.block
+
+    swap_router_abi = verbs.abi.load_abi("abi/SwapRouter.abi")
+    dai_abi = verbs.abi.load_abi("abi/dai.abi")
+    weth_erc20_abi = verbs.abi.load_abi("abi/WETHMintableERC20.abi")
+    uniswap_pool_abi = verbs.abi.load_abi("abi/UniswapV3Pool.abi")
+    uniswap_factory_abi = verbs.abi.load_abi("abi/UniswapV3Factory.abi")
+
+    net = verbs.envs.ForkEnv(
+        "https://eth-mainnet.g.alchemy.com/v2/{}".format(key),
+        0,
+        block_number,
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    )
+
+    fee = 3000
+    get_pool_args = uniswap_factory_abi.getPool.encode([WETH, DAI, fee])
+    pool_address = uniswap_factory_abi.getPool.call(
+        net,
+        net.admin_address,
+        verbs.utils.hex_to_bytes(UNISWAP_V3_FACTORY),
+        [WETH, DAI, fee],
+    )[0][0]
+
+    # Sanity check
+    assert pool_address == UNISWAP_WETH_DAI.lower()
+
+    # Initialize Uniswap agent
+    agent = UniswapAgent(
+        network=net,
+        dt=0.01,
+        fee=fee,
+        i=10,
+        mu=0.0,
+        sigma=0.3,
+        swap_router_abi=swap_router_abi,
+        swap_router_address=SWAP_ROUTER,
+        token_a_address=WETH,
+        token_a_price=2000.0,
+        token_b_address=DAI,
+        token_b_price=1.0,
+        uniswap_pool_abi=uniswap_pool_abi,
+        uniswap_pool_address=pool_address,
+    )
+
+    # mint and approve tokens
+    weth_erc20_abi.deposit.execute(
+        address=verbs.utils.hex_to_bytes(WETH),
+        args=[],
+        env=net,
+        sender=agent.address,
+        value=int(1e24),
+    )
+
+    weth_erc20_abi.approve.execute(
+        sender=agent.address,
+        address=verbs.utils.hex_to_bytes(WETH),
+        env=net,
+        args=[verbs.utils.hex_to_bytes(SWAP_ROUTER), int(1e24)],
+    )
+
+    dai_abi.mint.execute(
+        address=verbs.utils.hex_to_bytes(DAI),
+        sender=verbs.utils.hex_to_bytes(DAI_ADMIN),
+        env=net,
+        args=[agent.address, int(1e30)],
+    )
+
+    dai_abi.approve.execute(
+        sender=agent.address,
+        address=verbs.utils.hex_to_bytes(DAI),
+        env=net,
+        args=[verbs.utils.hex_to_bytes(SWAP_ROUTER), int(1e30)],
+    )
+
+    # run simulation
+    runner = verbs.sim.Sim(101, net, [agent])
+    results = runner.run(n_steps=100)
+    
+
+
