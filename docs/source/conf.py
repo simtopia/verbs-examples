3e32801709323cd2c8838ed5f537108d40eef874
zombie-einstein <13398815+zombie-einstein@users.noreply.github.com>
2024-02-23 20:04:35 +0000

    Tweak sphinx settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,33 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = "VERBS simulations"
 copyright = "2023, Simtopia"
 author = "Simtopia"
-# release = pkg_resources.get_distribution("verbs").version
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.coverage",
     "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.doctest",
+    "sphinx.ext.coverage",
     "sphinx.ext.viewcode",
     "sphinx_copybutton",
-    "sphinx.ext.intersphinx",
 ]
 
+napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
-add_module_names = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
+napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = True
 napoleon_use_param = True
-napoleon_use_ivar = True
+napoleon_use_ivar = False
+napoleon_use_rtype = False
+napoleon_preprocess_types = True
+napoleon_attr_annotations = True
+
+add_module_names = False
+
 autodoc_class_signature = "separated"
 
 templates_path = ["_templates"]
@@ -38,12 +36,8 @@ exclude_patterns = []
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "numpy": ("https://numpy.org/doc/stable", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
 }
-intersphinx_disabled_reftypes = ["*"]
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
 html_show_sourcelink = False
